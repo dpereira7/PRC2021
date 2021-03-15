14c315a1dfe3ceb4ff67fadec74efb961437c4eb
Diogo Pereira <35751445+dpereira7@users.noreply.github.com>
2021-03-15 14:33:11 +0000

    TPC3(fixed)

--- TPC's/TPC3/webApp/routes/index.js
+++ TPC's/TPC3/webApp/routes/index.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var axios = require('axios');
 
 const prefixes = `
-    PREFIX rdf: <w3.org/1999/02/22-rdf-syntax-ns#>
-    PREFIX owl: <w3.org/2002/07/owl#>
-    PREFIX rdfs: <w3.org/2000/01/rdf-schema#>
-    PREFIX noInferences: <ontotext.com/explicit>
-    PREFIX skos: <w3.org/2004/02/skos/core#>
+    PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+    PREFIX owl: <http://www.w3.org/2002/07/owl#>
+    PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+    PREFIX noInferences: <http://www.ontotext.com/explicit>
+    PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX adv: <http://www.di.uminho.pt/prc2021/Advogacia#>
 `
 
@@ -33,7 +33,7 @@ router.get('/repositorio/:id', function(req, res, next) {
   axios.get("http://localhost:7200/rest/repositories/" + req.params.id)
     .then(dados =>{
       var rep = dados.data;
-      console.log(rep)
+
       var getLink = "http://localhost:7200/repositories/" + req.params.id + "?query="
       var query = `SELECT ?s WHERE { ?s rdf:type owl:Class}`
 
@@ -42,7 +42,6 @@ router.get('/repositorio/:id', function(req, res, next) {
       axios.get(getLink + encoded)
         .then(dados => {
           classes = dados.data.results.bindings.map(bind => bind.s.value.split('#')[1]);
-
           res.render('repositorio', {rep: rep, prefixo: pre, classes: classes});   
         })
         .catch(err => {
@@ -67,7 +66,7 @@ router.get('/repositorio/:id/classe/:classe', function(req, res, next) {
     .then(dados => {
       var inds = dados.data.results.bindings.map(bind => bind.s.value.split('#')[1])
 
-      res.render('repositorio', {
+      res.render('classe', {
         repositorio: req.params.id,
         individuos: inds.sort(), 
         prefixo: pre,
@@ -80,11 +79,11 @@ router.get('/repositorio/:id/classe/:classe', function(req, res, next) {
 
 
 /* GET indivíduo de uma classe. */
-router.get('/repositorio/:id/classe/:classe/individuo/:id', function(req, res, next) {
-
+router.get('/repositorio/:id/classe/:classe/individuo/:ind', function(req, res, next) {
+  console.log(req.params)
   var getLink = "http://localhost:7200/repositories/" + req.params.id + "?query="
-  var query = `SELECT * WHERE { adv:` + req.params.individuo + `?p ?o}`
-
+  var query = `SELECT * WHERE { adv:` + req.params.ind + ` ?p ?o}`
+  
   var encoded = encodeURIComponent(prefixes + query)
 
   axios.get(getLink + encoded)
@@ -94,17 +93,18 @@ router.get('/repositorio/:id/classe/:classe/individuo/:id', function(req, res, n
             p: bind.p.value.split('#')[1],
             o: (bind.o.type == 'literal') ? bind.o.value : bind.o.value.split('#')[1]
         }
-    });
+      });
 
       res.render('individuo', {
         classe: req.params.classe,
-        individuo: req.params.individuo,
+        individuo: req.params.ind,
         prefixo: pre,
         propriedades: props
       });
-  })
-  .catch(err => {
-    res.render('error', {error: err});
+    })
+    .catch(err => {
+      console.log("aquiiiii!!!!")
+      res.render('error', {error: err});
   })
 });
 
